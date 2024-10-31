d02f10af85cc2fd205e9e2bdbb2b95e7ebaf8984
Archaeon <Archaeon@archaeon.top>
2024-10-31 23:59:05 +0800

    增加验证码配置

--- config/enter.go
+++ config/enter.go
@@ -1,10 +1,11 @@
 package config
 
 type Config struct {
-	System System `mapstructure:"system" json:"system" yaml:"system"`
-	Mysql  Mysql  `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
-	Redis  Redis  `mapstructure:"redis" json:"redis" yaml:"redis"`
-	Zap    Zap    `mapstructure:"zap" json:"zap" yaml:"zap"`
-	Cors   CORS   `mapstructure:"cors" json:"cors" yaml:"cors"`
-	JWT    JWT    `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
+	System  System  `mapstructure:"system" json:"system" yaml:"system"`
+	Mysql   Mysql   `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
+	Redis   Redis   `mapstructure:"redis" json:"redis" yaml:"redis"`
+	Zap     Zap     `mapstructure:"zap" json:"zap" yaml:"zap"`
+	Cors    CORS    `mapstructure:"cors" json:"cors" yaml:"cors"`
+	JWT     JWT     `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
+	Captcha Captcha `mapstructure:"captcha" json:"captcha" yaml:"captcha"`
 }
